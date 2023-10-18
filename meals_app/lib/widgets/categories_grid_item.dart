2da412e9f6f9984b30b9b8a5a8502883d68ed66e
Arunanthi Gunapalan <arun_99@hotmail.no>
2023-10-18 23:10:12 +0200

    created basic model for categories

---
+++ meals_app/lib/widgets/categories_grid_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+
+}

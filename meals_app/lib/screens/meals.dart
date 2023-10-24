26b95507f3bac9def906f06182265d6a07e62820
Arunanthi Gunapalan <arun_99@hotmail.no>
2023-10-24 15:48:19 +0200

    tapping on categories allows user to view the category content.

---
+++ meals_app/lib/screens/meals.dart
@@ -0,0 +1,45 @@
+// stateless because state doesnt need to be managed.
+import 'package:flutter/material.dart';
+import 'package:meals_app/main.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(
+        meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('List is empty.'),
+            const SizedBox(height: 16),
+            Text(
+              "Try selecting a different category.",
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content,
+      );
+    }
+  }

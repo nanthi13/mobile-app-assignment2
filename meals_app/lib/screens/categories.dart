7cc58a6223c464588f365c7ceabb4bc4024897b0
Arunanthi Gunapalan <arun_99@hotmail.no>
2023-10-26 21:45:00 +0200

    added tab based navigation

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -16,11 +16,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Choose a category'),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(24),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -33,7 +29,6 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(category: category, onSelectCategory: () {
               _selectCategory(context, category);
             },)
-          ]),
-    );
+          ]);
   }
 }
--- meals_app/lib/screens/meals.dart
+++ meals_app/lib/screens/meals.dart
@@ -6,9 +6,9 @@ import 'package:meals_app/screens/meal_details_screen.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -45,9 +45,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if(title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );

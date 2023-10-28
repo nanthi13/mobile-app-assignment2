b86bc03c19b24dd016b618173ae7c4d6ea64967b
Arunanthi Gunapalan <arun_99@hotmail.no>
2023-10-28 14:46:50 +0200

    added improvments to appdrawer and filters

--- meals_app/lib/screens/meal_details_screen.dart
+++ meals_app/lib/screens/meal_details_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 
-class MealDetailsScreen extends StatelessWidget{
-  const MealDetailsScreen ({super.key, required this.meal, required this.onToggleFavorite});
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen(
+      {super.key, required this.meal, required this.onToggleFavorite});
 
   final Meal meal;
   final void Function(Meal meal) onToggleFavorite;
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,7 +18,8 @@ class MealDetailsScreen extends StatelessWidget{
             onPressed: () {
               onToggleFavorite(meal);
             },
-            icon: const Icon(Icons.star),)
+            icon: const Icon(Icons.star),
+          )
         ],
       ),
       body: SingleChildScrollView(
@@ -28,44 +30,52 @@ class MealDetailsScreen extends StatelessWidget{
               height: 300,
               width: double.infinity,
               fit: BoxFit.cover,
-              ),
-              const SizedBox(height: 14),
-              Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold,
-              ),),
-      
-              const SizedBox(height: 14,),
-      
-              for (final ingredient in meal.ingredients)
-              Text(ingredient,
-               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),),
-              const SizedBox(height: 24),
+            ),
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final ingredient in meal.ingredients)
               Text(
-                'Steps',
-               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),),
-      
-               const SizedBox(height: 14,),
-      
-              for (final step in meal.steps)
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            const SizedBox(height: 24),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final step in meal.steps)
               Padding(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 12,
                   vertical: 8,
                 ),
-                child: Text(step,
-                textAlign: TextAlign.center,
-                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
               ),
           ],
         ),
-      ),      
+      ),
     );
   }
-} 
+}
